4c35994bd7a7f0ccac52d0b4b60c1d1a5e2dc1e3
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-04-23 20:52:18 +0800

    Implemented GWA comparison logic

--- highest_gwa/main_program.py
+++ highest_gwa/main_program.py
@@ -5,6 +5,12 @@ class GWAAnalyzer:
     def find_highest_gwa(self):
         try:
             with open(self.input_filename, mode='r') as gwa_records:
-                pass  # Logic coming next
-        except FileNotFoundError:
-            print("File not found.")
+                top_student_name = None
+                highest_gwa_value = 5.0
+
+            for line in gwa_records:
+                if line.strip():
+                name, gwa = line.strip().split(',')
+                if float(gwa) < highest_gwa_value:
+                    highest_gwa_value = float(gwa)
+                    top_student_name = name

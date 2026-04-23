a7d84bf2c70c5a836bdd3310c27149a5b0b9a8c6
jemmafaye13-hue <jemma.faye13@gmail.com>
2026-04-23 21:43:36 +0800

    Implemented while loop for continuous user input

--- multiple_line_writer/main_program.py
+++ multiple_line_writer/main_program.py
@@ -4,3 +4,15 @@ class MultiLineWriter:
 
     def start_writing(self):
         pass
+
+    def start_writing(self):
+        with open(self.output_file, "w") as file_handle:
+            more_lines = True
+            while more_lines:
+                user_input = input("Enter line: ")
+                file_handle.write(user_input + "\n")
+
+                choice = input("Are there more lines y/n? ").lower()
+                if choice != 'y':
+                    more_lines = False
+        print("Done! Lines have been saved.")
